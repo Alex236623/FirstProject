53e12cabd1884fd2058d2a21387adaebf3df4216
Dalest <alex236623@gmail.com>
2023-01-12 20:39:53 +0200

    test

---
+++ src/com/ua/robot/Main.java
@@ -0,0 +1,11 @@
+package com.ua.robot;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        test();
+    }
+    public static void test() {
+        System.out.println("Hello World");
+    }
+}
