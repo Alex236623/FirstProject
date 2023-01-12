764d2850f6852fe46488ac961dafe4e03f61484c
Dalest <alex236623@gmail.com>
2023-01-12 21:01:54 +0200

    test2

--- src/com/ua/robot/Main.java
+++ src/com/ua/robot/Main.java
@@ -4,8 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         test();
+        test2();
     }
     public static void test() {
+        System.out.println("Hello World");
+    }
+    public static void test2() {
         System.out.println("Hello World");
     }
 }
