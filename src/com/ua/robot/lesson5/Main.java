c9d7e43bb35a8823dbb6b437bb19998ec0b20747
Dalest <alex236623@gmail.com>
2023-01-26 12:31:06 +0200

    Homework lesson 5 added third task item

--- src/com/ua/robot/lesson5/Main.java
+++ src/com/ua/robot/lesson5/Main.java
@@ -26,6 +26,7 @@ public class Main extends Application {
         TextField text2 = new TextField();
         Button btn1 = new Button("Порівняти числа");
         Label label = new Label();
+        Label label2 = new Label();
 
         btn1.setLayoutX(100);
         btn1.setLayoutY(25);
@@ -35,17 +36,14 @@ public class Main extends Application {
         text2.setLayoutY(0);
         label.setLayoutX(100);
         label.setLayoutY(50);
+        label2.setLayoutX(100);
+        label2.setLayoutY(70);
 
         label.setVisible(false);
+        label2.setVisible(false);
 
-        text.textProperty().addListener((observable, oldValue, newValue) -> {
-            result1 = Integer.parseInt(newValue);
-            System.out.println(result1);
-        });
-        text2.textProperty().addListener((observable, oldValue, newValue2) -> {
-            result2 = Integer.parseInt(newValue2);
-            System.out.println(result2);
-        });
+        text.textProperty().addListener((observable, oldValue, newValue) -> result1 = Integer.parseInt(newValue));
+        text2.textProperty().addListener((observable, oldValue, newValue2) -> result2 = Integer.parseInt(newValue2));
 
         btn1.setOnAction(actionEvent -> {
 
@@ -56,11 +54,25 @@ public class Main extends Application {
             } else {
                 label.setText("Друге число більше першого");
             }
+
             label.setVisible(true);
+            label2.setVisible(true);
+
+            int remainderResult1 = result1 % 2;
+            int remainderResult2 = result2 % 2;
+            int uniqueAmount = remainderResult1++ + remainderResult1 + remainderResult2;
+            switch (uniqueAmount) {
+                case 1 -> label2.setText("обидва числа парні");
+                case 2 -> label2.setText("Перше число парне");
+                case 3 -> label2.setText("Друге число парне");
+                case 4 -> label2.setText("Обидва числа не парні");
+            }
+
+
         });
 
 
-        pane.getChildren().addAll(text, text2, btn1, label);
+        pane.getChildren().addAll(text, text2, btn1, label, label2);
         Scene primaryScene = new Scene(pane);
         primaryStage.setScene(primaryScene);
         primaryStage.show();
