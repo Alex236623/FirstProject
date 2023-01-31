9b3e223d790960d5b1591c3257e7590d15cea8c7
Dalest <alex236623@gmail.com>
2023-01-31 22:44:44 +0200

    Homework lesson 7

--- src/com/ua/robot/lesson7/Main.java
+++ src/com/ua/robot/lesson7/Main.java
@@ -4,7 +4,6 @@ package com.ua.robot.lesson7;
 public class Main {
     public static void main(String[] args) {
 
-
         int a = 7;
         int b = 3;
         int c = 200;
@@ -96,12 +95,3 @@ public class Main {
         return result.toString();
     }
 }
-
-
-
-
-
-
-
-
-
